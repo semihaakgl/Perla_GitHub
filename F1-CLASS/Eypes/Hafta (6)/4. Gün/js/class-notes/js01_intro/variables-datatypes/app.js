91187c3c26dc27ed89fe5eb8b0e9c9c4d48dcffd
semihaakgl <semiha.akgl60@gmail.com>
2023-10-05 23:11:04 +0300

    js

---
+++ F1-CLASS/Eypes/Hafta (6)/4. Gün/js/class-notes/js01_intro/variables-datatypes/app.js
@@ -0,0 +1,26 @@
+console.log("************data types")
+
+// ***************************CONST*******************
+
+const gs = "galatasaray";
+console.log(gs)//galatasaray
+console.log("gs") //gs
+console.log("galatasaray");
+
+const sayi = 20
+console.log(sayi)
+console.log(typeof sayi)
+console.log(sayi,typeof sayi)
+
+const sayi1 = "33"
+console.log(sayi1)
+// tırnak içinde olan her şey string (text) dir
+console.log(typeof sayi1)
+
+// const ile atanan sayı değerinden başka hiçbir değer atanmaz
+
+// const ile verilen değere başka değer verilemez
+// const id tc gibi özel bir defa değer verilir.
+const sayi = 3
+
+
